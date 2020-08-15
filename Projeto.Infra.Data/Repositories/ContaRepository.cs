ac41cf0f7f07173e4765f1851ad65557e0619c90
Fábio Cavadas <fabiocfferreira@gmail.com>
2020-08-15 19:56:44 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto.Infra.Data/Repositories/ContaRepository.cs
@@ -0,0 +1,24 @@
+﻿using Projeto.Infra.Data.Contexts;
+using Projeto.Infra.Data.Contracts;
+using Projeto.Infra.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Projeto.Infra.Data.Repositories
+{
+    public class ContaRepository : BaseRepository<Conta>, IContaRepository
+    {
+        private readonly DataContext dataContext;
+
+        public ContaRepository(DataContext dataContext) : base(dataContext) { this.dataContext = dataContext; }
+
+        public override List<Conta> GetAll()
+        {
+            return dataContext.Contas.OrderByDescending(c => c.DataConta).ToList();
+        }
+
+        public List<Conta> GetByDatas(DateTime dataMin, DateTime dataMax) { return dataContext.Contas.Where(c => c.DataConta >= dataMin && c.DataConta <= dataMax).OrderByDescending(c => c.DataConta).ToList(); }
+    }
+}
---
+++ Projeto.Presentation.MVC/Controllers/ContaController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Projeto.Infra.Data.Contracts;
+using Projeto.Infra.Data.Entities;
+using Projeto.Presentation.Mvc.Models;
+
+namespace Projeto.Presentation.Mvc.Controllers
+{
+    public class ContaController : Controller
+    {
+        private readonly IContaRepository contaRepository;
+
+        public ContaController(IContaRepository contaRepository)
+        {
+            this.contaRepository = contaRepository;
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(ContaCadastroModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var conta = new Conta
+                    {
+                        Id = Guid.NewGuid(),
+                        NomeConta = model.NomeConta,
+                        DataConta = DateTime.Parse(model.DataConta),
+                        ValorConta = decimal.Parse(model.ValorConta),
+                        Observacoes = model.Observacoes,
+                        Categoria = model.Categoria,
+                        FormaDePagamento = model.FormaDePagamento,
+                        Tipo = model.Tipo
+                    };
+
+                    contaRepository.Create(conta);
+
+                    TempData["MensagemSucesso"] = $"Conta {conta.NomeConta}, cadastrada com sucesso.";
+                    ModelState.Clear();
+                }
+                catch (Exception e)
+                {
+                    TempData["MensagemErro"] = e.Message;
+                }
+            }
+
+            return View();
+        }
+
+        public IActionResult Consulta()
+        {
+            return View();
+        }
+    }
+}
